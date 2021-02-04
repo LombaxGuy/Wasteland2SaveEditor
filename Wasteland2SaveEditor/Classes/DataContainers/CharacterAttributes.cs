5966bc80f8e2ead4dd75d6b4e51c6a41080e5561
Mikkel <mikkel@niila.io>
2021-02-04 17:52:59 +0100

    Changed: The method for converting a key to an index has been replaced by a Dictionary.

--- Wasteland2SaveEditor/Classes/DataContainers/CharacterAttributes.cs
+++ Wasteland2SaveEditor/Classes/DataContainers/CharacterAttributes.cs
@@ -1,33 +1,48 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Wasteland2SaveEditor.Extensions;
 
 namespace Wasteland2SaveEditor.Character
 {
     public class CharacterAttributes
     {
-        private Flag attributesFlag = new Flag("attributes");
-        private Flag pairFlag = new Flag("pair");
-        private Flag keyFlag = new Flag("key");
-        private Flag valueFlag = new Flag("value");
+        // flags use for seperating the data string
+        private readonly Flag attributesFlag = new Flag("attributes");
+        private readonly Flag pairFlag = new Flag("pair");
+        private readonly Flag keyFlag = new Flag("key");
+        private readonly Flag valueFlag = new Flag("value");
 
-        private Attribute[] all = new Attribute[7];
+        private readonly Attribute[] all = new Attribute[7];
 
-        public Attribute Coordination { get => all[0]; }
-        public Attribute Luck { get => all[1]; }
-        public Attribute Awareness { get => all[2]; }
-        public Attribute Strength { get => all[3]; }
-        public Attribute Speed { get => all[4]; }
-        public Attribute Intelligence { get => all[5]; }
-        public Attribute Charisma { get => all[6]; }
+        private readonly Dictionary<string, int> keyToIndex = new Dictionary<string, int>()
+        {
+            { "coordination",   0 },
+            { "luck",           1 },
+            { "awareness",      2 },
+            { "strength",       3 },
+            { "speed",          4 },
+            { "intelligence",   5 },
+            { "charisma",       6 }
+        };
 
         public Attribute[] All { get => all; }
+        public Attribute Coordination { get => all[keyToIndex["coordination"]]; }
+        public Attribute Luck { get => all[keyToIndex["luck"]]; }
+        public Attribute Awareness { get => all[keyToIndex["awareness"]]; }
+        public Attribute Strength { get => all[keyToIndex["strength"]]; }
+        public Attribute Speed { get => all[keyToIndex["speed"]]; }
+        public Attribute Intelligence { get => all[keyToIndex["intelligence"]]; }
+        public Attribute Charisma { get => all[keyToIndex["charisma"]]; }
 
+        /// <summary>
+        /// Returns a data string of the characters attributes.
+        /// </summary>
         public string Data
         {
             get
             {
                 string data = "";
 
+                // add each attribute to the data string in order
                 for (int i = 0; i < all.Length; i++)
                 {
                     data += all[i].Data;
@@ -37,48 +52,19 @@ namespace Wasteland2SaveEditor.Character
             }
         }
 
-        public CharacterAttributes(string rawCharacterData)
+        public CharacterAttributes(string data)
         {
-            string rawAttributeData = rawCharacterData.GetBetween(attributesFlag.Start, attributesFlag.End);
+            // finds the attributes and splits them up into seperate strings
+            string allAttributesString = data.GetBetween(attributesFlag.Start, attributesFlag.End);
+            string[] attributeStrings = allAttributesString.Split(pairFlag.End + pairFlag.Start);
 
-            string[] attributeData = rawAttributeData.Split(pairFlag.End + pairFlag.Start);
-
-            for (int i = 0; i < attributeData.Length; i++)
+            // loop through the strings and set the values of the 'all' array
+            for (int i = 0; i < attributeStrings.Length; i++)
             {
-                string key = attributeData[i].GetBetween(keyFlag.Start, keyFlag.End);
-                int value = int.Parse(attributeData[i].GetBetween(valueFlag.Start, valueFlag.End));
+                string key = attributeStrings[i].GetBetween(keyFlag.Start, keyFlag.End);
+                int value = int.Parse(attributeStrings[i].GetBetween(valueFlag.Start, valueFlag.End));
 
-                all[GetIndexFromKey(key)] = new Attribute(key, value);
-            }
-        }
-
-        private int GetIndexFromKey(string key)
-        {
-            switch (key)
-            {
-                case "coordination":
-                    return 0;
-
-                case "luck":
-                    return 1;
-
-                case "awareness":
-                    return 2;
-
-                case "strength":
-                    return 3;
-
-                case "speed":
-                    return 4;
-
-                case "intelligence":
-                    return 5;
-
-                case "charisma":
-                    return 6;
-
-                default:
-                    throw new Exception($"Attribute key '{key}' does not exist!");
+                all[keyToIndex[key]] = new Attribute(key, value);
             }
         }
     }
